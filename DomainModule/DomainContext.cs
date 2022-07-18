dbb4f2f2e0a92165beb84ca6e692bb007ae456e9
Dmitry <d.liapkin@gmail.com>
2022-07-18 18:50:13 +0300

    feat: added Domain module

---
+++ DomainModule/DomainContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace DomainModule
+{
+    class DomainContext : DbContext
+    {
+        public DomainContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Process> Processes { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
---
+++ DomainModule/Process.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DomainModule
+{
+    class Process
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public DateTime Begin { get; set; }
+        public DateTime? End { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Task> Tasks { get; set; }
+    }
+}
