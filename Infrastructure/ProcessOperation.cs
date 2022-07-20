8662b08924ff2335e2ca4fa861fb2305a5636fe0
Dmitry <d.liapkin@gmail.com>
2022-07-20 10:36:22 +0300

    feat: added create, update, delete operations for processes

---
+++ Infrastructure/ProcessOperation.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DomainModule.Repository;
+using DomainModule.Model;
+
+namespace Infrastructure
+{
+    public class ProcessOperation
+    {
+        public void CreateProcess(string name, string status, string beginDate, string endDate, string description)
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name");
+            }
+            if (String.IsNullOrEmpty(status))
+            {
+                throw new ArgumentNullException("status");
+            }
+            if (String.IsNullOrEmpty(beginDate))
+            {
+                throw new ArgumentNullException("beginDate");
+            }
+            else
+            {
+                if (!DateTime.TryParse(beginDate, out _))
+                {
+                    throw new ArgumentException("beginDate");
+                }
+            }
+            //if (String.IsNullOrEmpty(endDate))
+            //{
+            //    throw new ArgumentNullException("endDate");
+            //}
+            //else
+            //{
+            //    if (!DateTime.TryParse(endDate, out _))
+            //    {
+            //        throw new ArgumentException("endDate");
+            //    }
+            //}
+            if (description == null)
+            {
+                throw new ArgumentNullException("description");
+            }
+
+            Process process = new Process()
+            {
+                Name = name,
+                Status = status,
+                Begin = DateTime.Parse(beginDate),
+                End = null,
+                Description = description
+            };
+            if (DateTime.TryParse(endDate, out _))
+            {
+                process.End = DateTime.Parse(endDate);
+            }
+            UnitOfWork unitOfWork = new UnitOfWork();
+            unitOfWork.Processes.Create(process);
+            unitOfWork.Save();
+            unitOfWork.Dispose();
+        }
+
+        public void UpdateProcess(int id, string name, string status, string beginDate, string endDate, string description)
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name");
+            }
+            if (String.IsNullOrEmpty(status))
+            {
+                throw new ArgumentNullException("status");
+            }
+            if (String.IsNullOrEmpty(beginDate))
+            {
+                throw new ArgumentNullException("name");
+            }
+            else
+            {
+                if (!DateTime.TryParse(beginDate, out _))
+                {
+                    throw new ArgumentException("beginDate");
+                }
+            }
+            //if (String.IsNullOrEmpty(endDate))
+            //{
+            //    throw new ArgumentNullException("endDate");
+            //}
+            //else
+            //{
+            //    if (!DateTime.TryParse(endDate, out _))
+            //    {
+            //        throw new ArgumentException("endDate");
+            //    }
+            //}
+            if (description == null)
+            {
+                throw new ArgumentNullException("description");
+            }
+
+            UnitOfWork unitOfWork = new UnitOfWork();
+            Process process = unitOfWork.Processes.Get(id);
+            process.Name = name;
+            process.Status = status;
+            process.Begin = DateTime.Parse(beginDate);
+            process.Description = description;
+            if (DateTime.TryParse(endDate, out _))
+            {
+                process.End = DateTime.Parse(endDate);
+            }
+            unitOfWork.Processes.Update(process);
+            unitOfWork.Save();
+            unitOfWork.Dispose();
+        }
+
+        public void DeleteProcess(int id)
+        {
+            if (id < 0)
+            {
+                throw new ArgumentException("id");
+            }
+
+            UnitOfWork unitOfWork = new UnitOfWork();
+            Process process = unitOfWork.Processes.Get(id);
+            List<Task> tasksToDelete = process.Tasks.ToList();
+            foreach (Task task in tasksToDelete)
+            {
+                unitOfWork.Tasks.Delete(task.Id);
+            }
+            unitOfWork.Processes.Delete(process.Id);
+            unitOfWork.Save();
+            unitOfWork.Dispose();
+        }
+    }
+}
--- Infrastructure/Program.cs
+++ Infrastructure/Program.cs
@@ -10,6 +10,10 @@ namespace Infrastructure
     {
         static void Main(string[] args)
         {
+            //ProcessOperation operation = new ProcessOperation();
+            //operation.CreateProcess("Add NoSQL", "In Progress", "20.07.2022", "", "Install MogoDB");
+            //operation.UpdateProcess(3, "Remove NoSQL", "In Progress", "23.07.2022", "29.08.2022", "Uninstall MogoDB");
+            //operation.DeleteProcess(3);
         }
     }
 }
