e8db7b34f2b6c7e04068281a8c0a4ddfd45fd5ff
Dmitry <d.liapkin@gmail.com>
2022-07-28 13:59:36 +0300

    fix: fixed window styles and removed unused namespaces in views

--- AutoTask/MVVM/View/CreateProcessWindow.xaml.cs
+++ AutoTask/MVVM/View/CreateProcessWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using AutoTask.MVVM.ViewModel;
 
 namespace AutoTask.MVVM.View
@@ -29,6 +18,12 @@ namespace AutoTask.MVVM.View
         private void WindowClose(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            this.DragMove();
         }
     }
 }
