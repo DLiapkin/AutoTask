8a1dbb6da4ff2049514ee4aacfaa69be14ce1417
Dmitry <d.liapkin@gmail.com>
2022-08-02 11:37:18 +0300

    feat: modified Account ViewModel

--- AutoTask/MVVM/ViewModel/AccountViewModel.cs
+++ AutoTask/MVVM/ViewModel/AccountViewModel.cs
@@ -1,57 +1,35 @@
-﻿using AutoTask.UI.Core;
-using AutoTask.UI.MVVM.Model;
+﻿using AutoTask.UI.MVVM.Model;
 using AutoTask.Domain.Model;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace AutoTask.UI.MVVM.ViewModel
 {
     /// <summary>
     /// Represents View Model that controls Account View
     /// </summary>
-    public class AccountViewModel : ObservableObject
+    public partial class AccountViewModel : ObservableObject
     {
+        [ObservableProperty]
         private Account currentAccount;
+        [ObservableProperty]
         private User currentUser;
 
-        public Account CurrentAccount
-        {
-            get 
-            { 
-                return currentAccount; 
-            }
-            set 
-            { 
-                currentAccount = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public User CurrentUser
-        {
-            get 
-            { 
-                return currentUser; 
-            }
-            set 
-            { 
-                currentUser = value; 
-                OnPropertyChanged(); 
-            }
-        }
-
-        public RelayCommand EditCommand { get; set; }
-        public RelayCommand DeleteCommand { get; set; }
+        public ICommand EditCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
 
         public AccountViewModel()
         {
             CurrentAccount = new Account();
             CurrentUser = currentAccount.User;
 
-            EditCommand = new RelayCommand(o =>
+            EditCommand = new RelayCommand(() =>
             {
                 CurrentAccount.UpdateInfo(CurrentUser);
             });
 
-            DeleteCommand = new RelayCommand(o =>
+            DeleteCommand = new RelayCommand(() =>
             {
                 CurrentAccount.DeleteAccount(CurrentUser.Id);
                 CurrentAccount = new Account();
