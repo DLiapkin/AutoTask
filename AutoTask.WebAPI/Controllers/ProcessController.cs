50c4c258025471035ea319765115b9b45ce154b1
Dmitry <d.liapkin@gmail.com>
2022-09-02 20:27:24 +0300

    feat: added Web API project and Processes Controller

---
+++ AutoTask.WebAPI/Controllers/ProcessController.cs
@@ -0,0 +1,60 @@
+﻿using AutoTask.Shared;
+using AutoTask.Domain.Model;
+using AutoTask.Domain.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AutoTask.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProcessController : ControllerBase
+    {
+        private List<Process> _processes;
+
+        public ProcessController()
+        {
+            UnitOfWork unitOfWork = new UnitOfWork();
+            _processes = unitOfWork.Processes.GetAll().ToList();
+        }
+
+        // GET: api/<ProcessController>
+        [HttpGet]
+        public IEnumerable<Process> Get()
+        {
+            return _processes;
+        }
+
+        // GET api/<ProcessController>/5
+        [HttpGet("{id}")]
+        public Process Get(int id)
+        {
+            return _processes.First(p => p.Id == id);
+        }
+
+        // POST api/<ProcessController>
+        [HttpPost]
+        public void Post([FromBody] Process value)
+        {
+            ProcessOperation processOperation = new ProcessOperation();
+            processOperation.CreateProcess(value.Name, value.Begin, value.End, value.Description);
+        }
+
+        // PUT api/<ProcessController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Process value)
+        {
+            ProcessOperation processOperation = new ProcessOperation();
+            processOperation.UpdateProcess(id, value.Name, value.Begin, value.End, value.Description);
+        }
+
+        // DELETE api/<ProcessController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            ProcessOperation processOperation = new ProcessOperation();
+            processOperation.DeleteProcess(id);
+        }
+    }
+}
