19b985c98a4815755cdab12e3246f76da1ef6483
Dmitry <d.liapkin@gmail.com>
2022-09-05 20:14:11 +0300

    feat: added DI container configuring class and interface for Account

---
+++ AutoTask/MVVM/Model/ContainerConfig.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using AutoTask.UI.MVVM.Model;
+using AutoTask.UI.MVVM.Model.Interface;
+
+namespace AutoTask.UI.MVVM.Model
+{
+    public static class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Account>().As<IAccount>().SingleInstance();
+            builder.RegisterType<MainWindow>().AsSelf();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ AutoTask/MVVM/Model/Interface/IAccount.cs
@@ -0,0 +1,34 @@
+﻿using AutoTask.Domain.Model;
+
+namespace AutoTask.UI.MVVM.Model.Interface
+{
+    public interface IAccount
+    {
+        public User User { get; set; }
+        public bool IsLoggedIn { get; set; }
+        public string JwtToken { get; set; }
+
+        /// <summary>
+        /// Logs out current user
+        /// </summary>
+        public void LogOut();
+
+        /// <summary>
+        /// Logs in current user
+        /// </summary>
+        /// <param name="email">Email of current user</param>
+        /// <param name="password">Password of current user</param>
+        public void LogIn(string email, string password);
+
+        /// <summary>
+        /// Updates information about current user
+        /// </summary>
+        /// <param name="editedUser">User that contains new information</param>
+        public void UpdateInfo(User editedUser);
+
+        /// <summary>
+        /// Deletes current user
+        /// </summary>
+        public void DeleteAccount();
+    }
+}
